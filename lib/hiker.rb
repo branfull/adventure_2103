c4b971324f9000a60fa9f21e87d89f41cd2ea6d8
Brant Fuller <brant.fuller@gmail.com>
2021-04-28 10:33:56 -0500

    Create Hiker#initialize method

--- lib/hiker.rb
+++ lib/hiker.rb
@@ -1,4 +1,10 @@
 class Hiker
+  attr_reader :name,
+              :experience_level,
+              :snacks
   def initialize(name, experience_level)
+    @name = name
+    @experience_level = experience_level
+    @snacks = {}
   end
 end
