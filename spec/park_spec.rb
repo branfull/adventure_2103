831d231ab72a1604dd15895bd8fb4f023fd26485
Brant Fuller <brant.fuller@gmail.com>
2021-04-28 11:50:21 -0500

    Create test for Park#trail_names_at_level helper method

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -84,6 +84,28 @@ RSpec.describe Park do
       expect(park2.trails_at_level(:moderate)).to eq([trail4, trail6])
     end
   end
+  describe '#trail_names_at_level' do
+    trail1 = Trail.new({name: 'Grand Wash', length: '2.2 miles', level: :easy})
+    trail2 = Trail.new({name: 'Cohab Canyon', length: '1.7 miles', level: :moderate})
+    trail3 = Trail.new({name: 'Chimney Rock Loop', length: '3.6 miles', level: :strenuous})
+    trail4 = Trail.new({name: "Queen's/Navajo Loop", length: '2.9 miles', level: :moderate})
+    trail5 = Trail.new({name: 'Rim Trail', length: '11 miles', level: :easy})
+    trail6 = Trail.new({name: 'Tower Bridge', length: '3 miles', level: :moderate})
+    park1 = Park.new('Capitol Reef')
+    park2 = Park.new('Bryce Canyon')
+    park1.add_trail(trail1)
+    park1.add_trail(trail2)
+    park1.add_trail(trail3)
+    park2.add_trail(trail4)
+    park2.add_trail(trail5)
+    park2.add_trail(trail6)
+    expected1 = ['Cohab Canyon']
+    expected2 = ["Queen's/Navajo Loop", 'Tower Bridge']
+    it 'returns a list of all trails at a given level' do
+      expect(park1.trail_names_at_level(:moderate)).to eq(expected1)
+      expect(park2.trail_names_at_level(:moderate)).to eq(expected2)
+    end
+  end
   describe '#trails_by_level' do
     trail1 = Trail.new({name: 'Grand Wash', length: '2.2 miles', level: :easy})
     trail2 = Trail.new({name: 'Cohab Canyon', length: '1.7 miles', level: :moderate})
@@ -108,12 +130,12 @@ RSpec.describe Park do
       :moderate => ["Queen's/Navajo Loop", "Tower Bridge"],
       :easy => ["Rim Trail"]
     }
-    xit 'returns a list of all trails at a given level' do
+    it 'returns a list of all trails at a given level' do
       expect(park1.trails_by_level).to eq(expected1)
       expect(park2.trails_by_level).to eq(expected2)
     end
   end
-  describe '#trails_levels_in_park' do
+  describe '#trail_levels_in_park' do
     trail1 = Trail.new({name: 'Grand Wash', length: '2.2 miles', level: :easy})
     trail2 = Trail.new({name: 'Cohab Canyon', length: '1.7 miles', level: :moderate})
     trail3 = Trail.new({name: 'Chimney Rock Loop', length: '3.6 miles', level: :strenuous})
