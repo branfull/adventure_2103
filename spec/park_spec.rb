d533ec55d42b8266743870cefe4d3f348ae980f5
Brant Fuller <brant.fuller@gmail.com>
2021-04-28 11:37:01 -0500

    Create Park@trails_by_level test

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -84,4 +84,33 @@ RSpec.describe Park do
       expect(park2.trails_at_level(:moderate)).to eq([trail4, trail6])
     end
   end
+  describe '#trails_by_level' do
+    trail1 = Trail.new({name: 'Grand Wash', length: '2.2 miles', level: :easy})
+    trail2 = Trail.new({name: 'Cohab Canyon', length: '1.7 miles', level: :moderate})
+    trail3 = Trail.new({name: 'Chimney Rock Loop', length: '3.6 miles', level: :strenuous})
+    trail4 = Trail.new({name: "Queen's/Navajo Loop", length: '2.9 miles', level: :moderate})
+    trail5 = Trail.new({name: 'Rim Trail', length: '11 miles', level: :easy})
+    trail6 = Trail.new({name: 'Tower Bridge', length: '3 miles', level: :moderate})
+    park1 = Park.new('Capitol Reef')
+    park2 = Park.new('Bryce Canyon')
+    park1.add_trail(trail1)
+    park1.add_trail(trail2)
+    park1.add_trail(trail3)
+    park2.add_trail(trail4)
+    park2.add_trail(trail5)
+    park2.add_trail(trail6)
+    expected1 = {
+      :easy => ["Grand Wash"],
+      :moderate => ["Cohab Canyon"],
+      :strenuous => ["Chimney Rock Loop"]
+    }
+    expected2 = {
+      :moderate => ["Queen's/Navajo Loop", "Tower Bridge"],
+      :easy => ["Rim Trail"]
+    }
+    it 'returns a list of all trails at a given level' do
+      expect(park1.trails_by_level).to eq(expected1)
+      expect(park2.trails_by_level).to eq(expected2)
+    end
+  end
 end
