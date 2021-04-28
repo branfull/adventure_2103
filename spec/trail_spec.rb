9c41d34d0130419e36166caa8f87f490711d7b10
Brant Fuller <brant.fuller@gmail.com>
2021-04-28 10:08:04 -0500

    Create Trail#initialize tests

--- spec/trail_spec.rb
+++ spec/trail_spec.rb
@@ -2,9 +2,15 @@ require './lib/trail'
 
 RSpec.describe Trail do
   describe '#initialize' do
-    trail1 = Trail.new({name: 'Grand Wash', length: '2.2 miles', level: :easy})
+    trail1 = Trail.new({ name: 'Grand Wash', length: '2.2 miles', level: :easy })
     it 'exists' do
       expect(trail1).to be_an_instance_of(Trail)
+    end
+    it 'has a name' do
+      expect(trail1.name).to eq('Grand Wash')
+    end
+    it 'has a level' do
+      expect(trail1.level).to eq(:easy)
     end
   end
 end
