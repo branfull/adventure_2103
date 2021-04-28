448ce882475240e79ac8def78d06d629874d90a3
Brant Fuller <brant.fuller@gmail.com>
2021-04-28 10:32:16 -0500

    Create Hiker#initialize tests

--- spec/hiker_spec.rb
+++ spec/hiker_spec.rb
@@ -6,5 +6,14 @@ RSpec.describe Hiker do
     it 'exists' do
       expect(hiker).to be_an_instance_of(Hiker)
     end
+    it 'has a name' do
+      expect(hiker.name).to eq('Dora')
+    end
+    it 'has an experience level' do
+      expect(hiker.experience_level).to eq(:moderate)
+    end
+    it 'has a collection of snacks' do
+      expect(hiker.snacks).to eq({})
+    end
   end
 end
