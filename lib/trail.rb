2cd4d5a25c88df60239e2c25a8571f165ec9258e
Brant Fuller <brant.fuller@gmail.com>
2021-04-28 10:16:58 -0500

    Create Trail#length method

--- lib/trail.rb
+++ lib/trail.rb
@@ -4,5 +4,11 @@ class Trail
   def initialize(trail_params)
     @name = trail_params[:name]
     @level = trail_params[:level]
+    @length = trail_params[:length]
+  end
+
+  def length
+    length_components = @length.split(' ')
+    length_components[0].to_f
   end
 end
