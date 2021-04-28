adef6004142daba7cb4a4021cef197f03789b176
Brant Fuller <brant.fuller@gmail.com>
2021-04-28 11:14:48 -0500

    Create Park#trails_at_level helper method test

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -64,4 +64,24 @@ RSpec.describe Park do
       expect(park2.hikeable_miles).to eq(16.9)
     end
   end
+  describe '#trails_at_level' do
+    trail1 = Trail.new({name: 'Grand Wash', length: '2.2 miles', level: :easy})
+    trail2 = Trail.new({name: 'Cohab Canyon', length: '1.7 miles', level: :moderate})
+    trail3 = Trail.new({name: 'Chimney Rock Loop', length: '3.6 miles', level: :strenuous})
+    trail4 = Trail.new({name: "Queen's/Navajo Loop", length: '2.9 miles', level: :moderate})
+    trail5 = Trail.new({name: 'Rim Trail', length: '11 miles', level: :easy})
+    trail6 = Trail.new({name: 'Tower Bridge', length: '3 miles', level: :moderate})
+    park1 = Park.new('Capitol Reef')
+    park2 = Park.new('Bryce Canyon')
+    park1.add_trail(trail1)
+    park1.add_trail(trail2)
+    park1.add_trail(trail3)
+    park2.add_trail(trail4)
+    park2.add_trail(trail5)
+    park2.add_trail(trail6)
+    it 'returns a list of all trails at a given level' do
+      expect(park1.trails_at_level(:moderate)).to eq([trail2])
+      expect(park2.trails_at_level(:moderate)).to eq([trail4, trail6])
+    end
+  end
 end
