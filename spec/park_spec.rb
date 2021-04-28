2aeab8391e3235db9402cb9ed447eebde1ca77c4
Brant Fuller <brant.fuller@gmail.com>
2021-04-28 10:20:58 -0500

    Create Park#initialize tests

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe Park do
     it 'exists' do
       expect(park1).to be_an_instance_of(Park)
     end
+    it 'has a name' do
+      expect(park1.name).to eq('Capitol Reef')
+    end
+    it 'has trails' do
+      expect(park1.trails).to eq([])
+    end
   end
 end
