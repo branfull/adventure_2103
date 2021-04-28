a6662ab29af9e193e4be38b21af3faa209f9e59d
Brant Fuller <brant.fuller@gmail.com>
2021-04-28 10:54:09 -0500

    Create Park#trails_shorter_than test

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -24,4 +24,16 @@ RSpec.describe Park do
       expect(park1.trails).to eq([trail1, trail2])
     end
   end
+  describe '#trails_shorter_than' do
+    trail1 = Trail.new({name: 'Grand Wash', length: '2.2 miles', level: :easy})
+    trail2 = Trail.new({name: 'Cohab Canyon', length: '1.7 miles', level: :moderate})
+    trail3 = Trail.new({name: 'Chimney Rock Loop', length: '3.6 miles', level: :strenuous})
+    park1 = Park.new('Capitol Reef')
+    park1.add_trail(trail1)
+    park1.add_trail(trail2)
+    park1.add_trail(trail3)
+    it 'returns a list of trails shorter than a given distance' do
+      expect(park1.trails_shorter_than(2.5)).to eq([trail1, trail2])
+    end
+  end
 end
